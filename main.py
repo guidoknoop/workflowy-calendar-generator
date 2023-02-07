53db5bc6fdaa6726e299fd2325604231e5aef98e
Guido Knoop <guidoknoop@outlook.com>
2023-02-07 12:57:34 +0100

    Added support for WorkFlowy dates

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ LOCALE = 'en_US'            # Which locale would you like to use? Choose from ht
 year = 2023                 # For which year would you like to generate a calendar?
 display_date = '%m-%d-%Y'   # How would you like to display the date? Choose from https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
 display_week = True         # Set to True if you want to see the number of the week as a tag, False if not.
+workflowy_dates = True      # Do you want to use that date system of WorkFlowy for improved functionality?
 
 # Don't change anything after this line
 locale.setlocale(locale.LC_ALL, LOCALE)
@@ -19,24 +20,31 @@ def daterange(start_date, end_date):
 start_date = date(year, 1, 1)
 end_date = date(year + 1, 1, 1)
 
-html = f'<?xml version="1.0"?><opml version="2.0"><body><outline text="{year}">'
+opml = f'<?xml version="1.0"?><opml version="2.0"><body><outline text="{year}">'
 
 for single_date in daterange(start_date, end_date):
-    if single_date.day == 1:
-        html += f'<outline text="{single_date.strftime("%B").upper()}">'
-
-    if not display_week:
-        html += f'<outline text="{single_date.strftime(display_date)}" _note="{single_date.strftime("%a")}" />'
-    else:
-        html += f'<outline text="{single_date.strftime(display_date)}" _note="{single_date.strftime("%a")} | #wk{single_date.strftime("%W")}-{single_date.strftime("%y")}" />'
-
     day = single_date.day
     month = single_date.month
     year = single_date.year
 
+    if day == 1:
+        opml += f'<outline text="{single_date.strftime("%B").upper()}">'
+
+    if workflowy_dates:
+        if not display_week:
+            opml += f'<outline text="&lt;time startYear=&quot;{year}&quot; startMonth=&quot;{month}&quot; startDay=&quot;{day}&quot;&gt;{single_date.strftime(display_date)}&lt;/time&gt;" _note="{single_date.strftime("%a")}"/>'
+        else:
+            opml += f'<outline text="&lt;time startYear=&quot;{year}&quot; startMonth=&quot;{month}&quot; startDay=&quot;{day}&quot;&gt;{single_date.strftime(display_date)}&lt;/time&gt;" _note="{single_date.strftime("%a")} | #wk{single_date.strftime("%W")}-{single_date.strftime("%y")}" />'
+        
+    else:
+        if not display_week:
+            opml += f'<outline text="{single_date.strftime(display_date)}" _note="{single_date.strftime("%a")}" />'
+        else:
+            opml += f'<outline text="{single_date.strftime(display_date)}" _note="{single_date.strftime("%a")} | #wk{single_date.strftime("%W")}-{single_date.strftime("%y")}" />'
+
     if day == calendar.monthrange(year, month)[1]:
-        html += '</outline>'
+        opml += '</outline>'
 
-html += '</outline></body></opml>'
+opml += '</outline></body></opml>'
 
-clipboard.copy(html)
+clipboard.copy(opml)
