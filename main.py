4f8b6292da4af7096ee88807ad14272c70603ab3
Guido Knoop <guidoknoop@outlook.com>
2024-06-03 16:36:15 +0200

    Added smart date support

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from datetime import timedelta, date
 import locale
 
 # Settings
-LOCALE = 'en_US'            # Which locale would you like to use? Choose from https://www.localeplanet.com/icu/
+LOCALE = 'en_EN'            # Which locale would you like to use? Choose from https://www.localeplanet.com/icu/
 year = 2024                 # For which year would you like to generate a calendar?
-display_date = '%m-%d-%Y'   # How would you like to display the date? Choose from https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
+display_date = '%m-%d-%Y'   # How would you like to display the date? Choose from https://docs.python.org/3/library/datetime.opml#strftime-and-strptime-format-codes
 display_week = True         # Set to True if you want to see the number of the week as a tag, False if not.
-workflowy_dates = True      # Do you want to use that date system of WorkFlowy for improved functionality?
 
 # Don't change anything after this line
 locale.setlocale(locale.LC_ALL, LOCALE)
@@ -23,24 +22,16 @@ end_date = date(year + 1, 1, 1)
 opml = f'<?xml version="1.0"?><opml version="2.0"><body><outline text="{year}">'
 
 for single_date in daterange(start_date, end_date):
+    if single_date.day == 1:
+        opml += f'<outline text="{single_date.strftime("%B").upper()}">'
+
+    if not display_week:
+        opml += f'<outline text="{single_date.strftime(display_date)}" _note="{single_date.strftime("%a")}" />'
+    else:
+        opml += f'<outline text="&lt;time startYear=&quot;{single_date.year}&quot; startMonth=&quot;{single_date.month}&quot; startDay=&quot;{single_date.day}&quot;&gt;{single_date.strftime(display_date)}&lt;/time&gt; " _note="{single_date.strftime("%a")} | #wk{single_date.strftime("%W")}-{single_date.strftime("%y")}" />'
     day = single_date.day
     month = single_date.month
     year = single_date.year
-
-    if day == 1:
-        opml += f'<outline text="{single_date.strftime("%B").upper()}">'
-
-    if workflowy_dates:
-        if not display_week:
-            opml += f'<outline text="&lt;time startYear=&quot;{year}&quot; startMonth=&quot;{month}&quot; startDay=&quot;{day}&quot;&gt;{single_date.strftime(display_date)}&lt;/time&gt;" _note="{single_date.strftime("%a")}"/>'
-        else:
-            opml += f'<outline text="&lt;time startYear=&quot;{year}&quot; startMonth=&quot;{month}&quot; startDay=&quot;{day}&quot;&gt;{single_date.strftime(display_date)}&lt;/time&gt;" _note="{single_date.strftime("%a")} | #wk{single_date.strftime("%W")}-{single_date.strftime("%y")}" />'
-        
-    else:
-        if not display_week:
-            opml += f'<outline text="{single_date.strftime(display_date)}" _note="{single_date.strftime("%a")}" />'
-        else:
-            opml += f'<outline text="{single_date.strftime(display_date)}" _note="{single_date.strftime("%a")} | #wk{single_date.strftime("%W")}-{single_date.strftime("%y")}" />'
 
     if day == calendar.monthrange(year, month)[1]:
         opml += '</outline>'
